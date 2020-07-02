5f34bb27524276ccc09e0e84d8605caf0fdad8ab
ValdiviesoM9 <66709852+ValdiviesoM9@users.noreply.github.com>
2020-07-02 16:07:35 -0500

    Create Calcular_Edad.php

---
+++ Calcular_Edad.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Calcula tu edad</title>
+	</head>
+	<body>
+		<h1> Ver tu edad actual</h1>
+		<form name="forma" method="POST" action="./Calculo_Edad.php">
+			<label form= "Fecha_Nacimiento" >Fecha de nacimiento:</label>
+			<input type= "date" name= Fecha_Nacimiento value=""/>
+			<input type= "submit" value="Ver"/>
+		</form>
+	</body>
+
+</html>
