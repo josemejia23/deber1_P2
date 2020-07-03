e0045e283a02dc247f07bf103f7a07c864df28f1
ValdiviesoM9 <66709852+ValdiviesoM9@users.noreply.github.com>
2020-07-03 13:50:30 -0500

    Update Calculo_Edad.php

--- ejercicio3/Calculo_Edad.php
+++ ejercicio3/Calculo_Edad.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <?php
-$Fecha_Nacimiento = $_POST["Fecha_Nacimiento"];
-$fecha_actual = date("d-m-Y");
-$res = strtotime($fecha_actual) - strtotime($Fecha_Nacimiento);
-$edad = intval($res / 60 / 60 / 24 / 365.25);
-
+	$Fecha_Nacimiento = $_POST["Fecha_Nacimiento"];
+	$fecha_actual = date("d-m-Y");
+	$edad=edad_calculo($Fecha_Nacimiento,$fecha_actual);	
+		
+	function edad_calculo($fechaIN,$fechaFI){
+		$inicio = date_create($fechaIN);
+		$fin = date_create($fechaFI);
+		$interval = date_diff($inicio,$fin);
+		
+		$tiempo=array();
+		foreach($interval as $resp){
+			$tiempo[]=$resp;
+		}
+		return $tiempo;
+	}
 ?>
 <html>
 
@@ -15,7 +25,7 @@ $edad = intval($res / 60 / 60 / 24 / 365.25);
 <body>
 	<div align="center">
 		<h1> Edad actual </h1>
-		<p> Desde <?= $Fecha_Nacimiento ?> hasta hoy tienes una edad de <?= $edad ?> años</p>
+		<p> Desde <?= $Fecha_Nacimiento?> hasta hoy tienes una edad de <?= $edad[0]?> años con <?= $edad[1]?> meses y <?= $edad[2]?> dias</p>
 		<button class="enlace" role="link" onclick="window.location='../index.html'">Volver Al Menú Principal</button>
 	</div>
 
